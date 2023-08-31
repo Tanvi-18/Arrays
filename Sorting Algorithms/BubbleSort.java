5c4826a66fb7f1e66396847508959ec12482a462
Tanvi-18 <tanvi18parab@gmail.com>
2023-09-01 00:12:49 +0530

    Sorting Algorithms

--- Sorting Algorithms/BubbleSort.java
+++ Sorting Algorithms/BubbleSort.java
@@ -4,11 +4,11 @@ public class BubbleSort {
 	
 	public static int[] sortedArray(int arr[]) {
 		for(int i=0; i<arr.length; i++) {
-			for(int j = i+1; j<arr.length; j++) {
-				if(arr[i] > arr[j]) {
-					int temp = arr[i];
-					  arr[i] = arr[j];
-					  arr[j] = temp;
+			for(int j=0; j<arr.length-1-i; j++) {
+				if(arr[j] > arr[j+1]) {
+					int temp = arr[j];
+					  arr[j] = arr[j+1];
+					  arr[j+1] = temp;
 				}
 			}
 		}
@@ -25,3 +25,4 @@ public class BubbleSort {
 	}
 	
 }
+
--- Sorting Algorithms/InsertionSort.java
+++ Sorting Algorithms/InsertionSort.java
@@ -2,26 +2,27 @@ package com.jsp.SortingAlgorithms;
 
 public class InsertionSort {
 	
-	public static int[] sortedArray(int arr[]) {
-		for(int i=0; i<arr.length; i++) {
-			for(int j = 0; j<arr.length-1-i; j++) {
-				if(arr[j] > arr[j+1]) {
-					int temp = arr[j];
-					  arr[j] = arr[j+1];
-					  arr[j+1] = temp;
-				}
+	public static void sortedArray(int arr[]) {
+		for(int i=1; i<arr.length; i++) {
+			int current=arr[i];
+			int j=i-1;
+			
+			while(j>=0 && current<arr[j]) {
+				arr[j+1] = arr[j];
+				j--;
 			}
+			arr[j+1]=current;
 		}
-		return arr;
 	}
 
 		
 	public static void main(String[] args) {
 		int arr[] = {7, 4, 1, 5, 2, 6, 3, 8};
-		int res[] = sortedArray(arr);
-		for(int i =0; i<res.length; i++) {
-			System.out.print(res[i] + " ");
+		sortedArray(arr);
+		for(int i =0; i<arr.length; i++) {
+			System.out.print(arr[i] + " ");
 		}
 	}
 	
 }
+
