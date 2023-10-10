0101f52fd01b5a496d3cb1ce660edfcbe27e1da8
Tanvi-18 <tanvi18parab@gmail.com>
2023-10-10 21:29:42 +0530

    Sorting Algorithms

--- Sorting Algorithms/InsertionSort.java
+++ Sorting Algorithms/InsertionSort.java
@@ -2,8 +2,9 @@ package com.jsp.SortingAlgorithms;
 
 public class InsertionSort {
 	
-	public static void sortedArray(int arr[]) {
-		for(int i=1; i<arr.length; i++) {
+	public static int[] insertionSort(int arr[]) {
+		
+		for(int i=1; i<arr.length; i++) {	
 			int current=arr[i];
 			int j=i-1;
 			
@@ -12,16 +13,17 @@ public class InsertionSort {
 				j--;
 			}
 			arr[j+1]=current;
-		}
+			}
+		return arr;
 	}
-
-		
+	
 	public static void main(String[] args) {
 		int arr[] = {7, 4, 1, 5, 2, 6, 3, 8};
-		sortedArray(arr);
-		for(int i =0; i<arr.length; i++) {
-			System.out.print(arr[i] + " ");
+		int res[] = insertionSort(arr);
+		for(int i =0; i<res.length; i++) {
+			System.out.print(res[i] + " ");
 		}
+
 	}
 	
 }
